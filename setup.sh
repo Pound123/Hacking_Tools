8b509558341ea88ff827fbc36c261d912966470b
P'Pound <75177075+Pound123@users.noreply.github.com>
2021-09-14 16:27:42 +0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-clear
-apt update -y && apt upgrade -y && apt install git -y && apt install python -y && pip install lolcat && gem install lolcat && ternux-setup-stroage
-mv tools ~/../usr/bin && chmod 777 ~/../usr/bin/tools
-mkdir Data && mv nikto Data
-clear
-cat Banner_install.txt | lolcat
